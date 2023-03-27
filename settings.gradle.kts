5d65ab4d0c2c4ff5f8ebd158fd9520255eadff30
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-03-27 21:55:38 +0200

    upgrade: RefreshVersions 0.51.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 rootProject.name = "gitlab-ci.kt"
 
 plugins {
-    id("de.fayard.refreshVersions") version "0.50.2"
-////                            # available:"0.51.0"
+    id("de.fayard.refreshVersions") version "0.51.0"
 }
