f707f0258c5916f0b3bde682a5318b6893267709
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-07-30 21:16:48 +0200

    docs: Document the Pacman plugin

--- gitlab-ci-kotlin/src/commonMain/kotlin/plugins/Pacman.kt
+++ gitlab-ci-kotlin/src/commonMain/kotlin/plugins/Pacman.kt
@@ -19,13 +19,39 @@ package opensavvy.gitlab.ci.plugins
 import opensavvy.gitlab.ci.script.CommandDsl
 import opensavvy.gitlab.ci.script.shell
 
+/**
+ * Integrates [Pacman](https://wiki.archlinux.org/title/Pacman) into your build.
+ *
+ * This plugin adds the [pacman] extension to scripts:
+ *
+ * ```kotlin
+ * val deploy by job {
+ *     image("archlinux:latest")
+ *
+ *     script {
+ *         pacman.sync("git")
+ *         shell("git describe")
+ *     }
+ * }
+ * ```
+ */
 class Pacman private constructor(private val dsl: CommandDsl) {
 
+	/**
+	 * Executes `pacman -Syuu` [pkg].
+	 */
 	fun sync(vararg pkg: String) = with(dsl) {
 		shell("pacman -Syuu ${pkg.joinToString(separator = " ")}")
 	}
 
 	companion object {
+		/**
+		 * Accesses Pacman commands.
+		 *
+		 * To use this plugin, the `pacman` command must be available to this job.
+		 *
+		 * @see Pacman
+		 */
 		val CommandDsl.pacman get() = Pacman(this)
 	}
 }
